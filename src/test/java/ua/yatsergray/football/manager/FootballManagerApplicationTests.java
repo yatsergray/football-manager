085091d21652783b65328400fcd055356f242108
Yezhyck <sergeysergeevytch@gmail.com>
2025-01-02 20:03:14 +0200

    Initial commit

---
+++ src/test/java/ua/yatsergray/football/manager/FootballManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.yatsergray.football.manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FootballManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
